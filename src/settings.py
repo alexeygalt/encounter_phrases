caa152b57f3f14e1470cddb3c1a8cd0b984891e4
alexeygalt <alexeygalt@gmail.com>
2024-09-17 10:59:39 +0300

    getter env changed

--- src/settings.py
+++ src/settings.py
@@ -1,11 +1,3 @@
 import os
-from pathlib import Path
-import environ
 
-BASE_DIR = Path(__file__).resolve().parent
-
-environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
-
-env = environ.Env()
-
-BOT_TOKEN = env.str("BOT_TOKEN", None)
+BOT_TOKEN = os.environ['BOT_TOKEN']
